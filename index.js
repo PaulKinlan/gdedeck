052c23ae537100d945b97ed8295813998678f30d
Paul Kinlan <paul.kinlan@gmail.com>
2019-04-10 17:54:24 +0100

    Fixing paths

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+/**
+ * Copyright 2018 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const {Server, FeedFetcher} = require('topicdeck');
+const path = require('path');
 
 const fetchInterval = 60 * 60 * 1000;
-const feedFetcher = new FeedFetcher(fetchInterval, __dirname + '/config/');
+const feedFetcher = new FeedFetcher(fetchInterval, path.join(__dirname, 'config'));
 
 const server = new Server({
-  assetPathBase: `node_modules/topicdeck/dist/server/`,
-  overridePathBase: `${__dirname}/`,
-  dataPath: `${__dirname}/config/`
+  assetPathBase:  path.join(__dirname, 'topicdeck', 'server'),
+  dataPath: path.join(__dirname, 'config'),
+  overridePathBase: path.join(__dirname, '.')
 }, feedFetcher);
 
 server.start(8080);
